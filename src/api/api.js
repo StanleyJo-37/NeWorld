d75bd1ac8d05462fc12d0fb5a005a551896e77cd
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2024-11-30 10:25:18 +0700

    # feat create api call

---
+++ src/api/api.js
@@ -0,0 +1,21 @@
+import { Axios } from "axios";
+
+const baseURL = "https://restcountries.com/v3.1";
+
+const axios = new Axios({
+    baseURL
+})
+
+export async function getAllCountries() {
+    return await axios.request({
+        url: 'all',
+        method: 'GET',
+    });
+}
+
+export async function getCountry(name) {
+    return await axios.request({
+        url: `name/${name}`,
+        method: 'GET',
+    });
+}
