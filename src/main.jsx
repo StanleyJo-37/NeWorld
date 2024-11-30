1e0a9c8fe4a40c8d08f5705ecc74f38a94c13d59
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2024-11-30 09:55:11 +0700

    # feat add providers

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ThemeProvider from './providers/ThemeProvider.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import routes from './constants/routes.jsx'
+import { ToastContainer } from 'react-toastify'
+
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+      <RouterProvider router={router} />
+      <ToastContainer />
+    </ThemeProvider>
   </StrictMode>,
 )
