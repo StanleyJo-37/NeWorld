bfcb760b6c34d7c698bc9346a8d7f0448978b815
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2024-11-30 22:56:42 +0700

    # remove ThemeProviders

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import ThemeProvider from './providers/ThemeProvider.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import routes from './constants/routes.jsx'
-import { ToastContainer } from 'react-toastify'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import routes from './constants/routes.jsx';
+import './index.css';
+
 
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-      <RouterProvider router={router} />
-      <ToastContainer />
-    </ThemeProvider>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
