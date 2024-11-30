f1bd6aa90746768191057ee6123e095920ff098c
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2024-11-30 23:00:27 +0700

    # feat light and dark mode toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,58 @@
-import { Link } from "react-router-dom";
-import { NavItem } from "../types/Navbar";
+import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import clsx from "clsx";
-import { useEffect, useState } from "react";
 import * as Switch from "@radix-ui/react-switch";
+import PropType from "prop-types";
+import "../styles/navbar.css";
 
-export default function Navbar({ navItems }) {
-    const [pathname, setPathname] = useState<string>('');
-    
-    useEffect(() => {
-        setPathname(window.location.href);
-    }, [window.location.href]);
+export default function Navbar({ navItems, _theme }) {
+    const location = useLocation();
+    const [theme, setTheme] = useState(_theme);
+
+    const toggleTheme = () => {
+        if (theme === "dark") {
+            setTheme("light");
+            document.documentElement.setAttribute('data-theme', 'light');
+            localStorage.setItem('theme', 'light');
+        }
+        else {
+            setTheme("dark");
+            document.documentElement.setAttribute('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark');
+        }
+    }
 
     return (
         <nav className="navbar">
-            {
-                navItems.map(navItem => (
-                    <Link
-                        to={navItem.url}
-                        className={clsx({
-                            "active-nav-item": pathname === navItem.url,
-                        })}
-                    >
-                        {navItem.icon && <img src={navItem.icon} className="nav-item-icon" />}
-                        <p>{navItem.label}</p>
-                    </Link>
-                ))
-            }
+            <div className="nav-choices">
+                {
+                    navItems.map((navItem, i) => (
+                        <Link
+                            to={navItem.path}
+                            className={clsx({
+                                "active-nav-item": location.pathname === navItem.path,
+                                "ml-12": i > 0,
+                            })}
+                            key={navItem.label}
+                        >
+                            {navItem.icon && <img src={navItem.icon} className="nav-item-icon" />}
+                            <p>{navItem.label}</p>
+                        </Link>
+                    ))
+                }
+            </div>
             <Switch.Root
-                onChange={() => {
-                    
-                }}
+                onClick={toggleTheme}
+                className="SwitchRoot"
+                checked={theme === "light"}
             >
-            
+                <Switch.Thumb className="SwitchThumb" />
             </Switch.Root>
         </nav>
     );
+}
+
+Navbar.propTypes = {
+    navItems: PropType.arrayOf(PropType.object.isRequired).isRequired,
+    _theme: PropType.string,
 }
