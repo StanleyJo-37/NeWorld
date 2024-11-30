c463223b011d3b9ca00d83e740fae6350b9f1f4e
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2024-11-30 22:57:06 +0700

    # feat constants for navbar items

---
+++ src/constants/nav-items.jsx
@@ -0,0 +1,14 @@
+export const navItems = [
+    {
+        label: 'Home',
+        path: '/',
+    },
+    {
+        label: 'Countries',
+        path: '/countries',
+    },
+    {
+        label: 'About',
+        path: '/about',
+    },
+]
