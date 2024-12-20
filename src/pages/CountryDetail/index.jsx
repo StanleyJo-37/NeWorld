ec9922ad0b8aecef6c048cf1e2486e71847ba639
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2024-12-20 23:51:24 +0700

    # feat country details

--- src/pages/CountryDetail/index.jsx
+++ src/pages/CountryDetail/index.jsx
@@ -3,7 +3,6 @@ import { getCountry } from "../../api/api";
 import { AxiosError } from "axios";
 import { toast } from "react-toastify";
 import { useParams } from "react-router-dom";
-import { MapContainer, TileLayer, } from 'react-leaflet';
 import "./../../styles/country-detail.css"
 
 export default function Index() {
@@ -30,28 +29,155 @@ export default function Index() {
 
     useEffect(() => {
         fetchCountry();
-        if (country) console.log(country.name);
     }, []);
 
-    const renderMaps = () => {
-        if (!country) return <></>
-        
-        return (
-            <MapContainer className="map" center={[51.505, -0.09]} zoom={13} scrollWheelZoom >
-                <TileLayer
-                    // attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url={country.maps.googleMaps}
-                />
-            </MapContainer>
-        );
-    }
-
     return (
-        <div>
-            {/* {renderMaps()} */}
-            {isLoading
-            ? "Loading..."
-            : <div>{country.name.common}</div>}
-        </div>
+        isLoading
+        ? (
+            <div>
+                Loading...
+            </div>
+        )
+        : (
+            <div
+                style={{
+                    paddingTop: 16,
+                    paddingBottom: 16,
+                    paddingLeft: 64,
+                    paddingRight: 64,
+                }}
+            >
+                <div
+                    style={{
+                        marginBottom: 32,
+                    }}
+                >
+                    <h1
+                        style={{
+                            fontFamily: 'Nunito-Bold',
+                            fontSize: 36,
+                        }}
+                    >
+                        {country.name.common} ({country.cca3})
+                    </h1>
+                    <h2
+                        style={{
+                            fontFamily: 'Nunito-Medium',
+                            fontSize: 24,
+                            marginBottom: 24,
+                        }}
+                    >
+                        {country.name.official}
+                    </h2>
+                    <img
+                        src={country.flags.png}
+                        alt={`${country.name.common}'s flag: ${country.flags.alt}`}
+                        className="flag-img"
+                        style={{
+                            width: '300px',
+                        }}
+                    />
+                </div>
+                <div>
+                    <p>Currencies:</p>
+                    <ul
+                        style={{
+                            paddingLeft: 20,
+                        }}
+                    >
+                        {
+                            Object.keys(country.currencies).map(key => (
+                                    <li
+                                        key={key}
+                                    >
+                                        {country.currencies[key].name} 	&#40;{country.currencies[key].symbol}&#41;
+                                    </li>
+                                )
+                            )
+                        }
+                    </ul>
+
+                    <p>Capitals:</p>
+                    <ul
+                        style={{
+                            paddingLeft: 20,
+                        }}
+                    >
+                        {
+                            country.capital.map(capital => (
+                                    <li
+                                        key={capital}
+                                    >
+                                        {capital}
+                                    </li>
+                                )
+                            )
+                        }
+                    </ul>
+
+                    <p>Population: {country.population} people</p>
+
+                    <p>Timezones:</p>
+                    <ul
+                        style={{
+                            paddingLeft: 20,
+                        }}
+                    >
+                        {
+                            country.timezones.map(timezone => (
+                                    <li
+                                        key={timezone}
+                                    >
+                                        {timezone}
+                                    </li>
+                                )
+                            )
+                        }
+                    </ul>
+
+                    <p>Region: {country.region}</p>
+                    <p>Subregion: {country.subregion}</p>
+
+                    <p>Continents:</p>
+                    <ul
+                        style={{
+                            paddingLeft: 20,
+                        }}
+                    >
+                        {
+                            country.continents.map(continent => (
+                                    <li
+                                        key={continent}
+                                    >
+                                        {continent}
+                                    </li>
+                                )
+                            )
+                        }
+                    </ul>
+
+                    <p>Languages:</p>
+                    <ul
+                        style={{
+                            paddingLeft: 20,
+                        }}
+                    >
+                        {
+                            Object.keys(country.languages).map(key => (
+                                    <li
+                                        key={key}
+                                    >
+                                        {country.languages[key]}
+                                    </li>
+                                )
+                            )
+                        }
+                    </ul>
+
+                    <p>Latitude: {country.latlng[0]}</p>
+                    <p>Longitude: {country.latlng[1]}</p>
+                </div>
+            </div>
+        )
     );
 }
