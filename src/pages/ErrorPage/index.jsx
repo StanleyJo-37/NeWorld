aed418f7a181e7daf810ea1a83dc5cd49e3b7fda
StanleyJo-37 <stanley.wahjudi@binus.ac.id>
2024-12-21 00:11:17 +0700

    # feat error page

---
+++ src/pages/ErrorPage/index.jsx
@@ -0,0 +1,11 @@
+import "./../../styles/home.css";
+import './../../index.css';
+
+export default function Index() {
+
+    return (
+        <div className="container home-container">
+            <p>ERROR WOY</p>
+        </div>
+    );
+}
